0239c1237cb22fe30feb4e1afb259fe4e7fb14e4
[Emmanard] <Emmanard9@gmail.com>
2025-11-01 02:08:39 +0100

    ✨ feat(socket): implement message delivery and read receipt system

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,11 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173",
-        "http://127.0.0.1:5500",  // Live Server
-      "http://localhost:5500"
-
-    ]  
+    origin: ["http://localhost:5173"]  
   },
 });
 
